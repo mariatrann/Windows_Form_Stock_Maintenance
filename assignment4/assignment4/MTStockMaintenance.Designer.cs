d507a7f442a6314abb24b8222bfc4aac17de0c0b
mariatrann <98122407+mariatrann@users.noreply.github.com>
2022-03-05 15:42:37 -0500

    Initial
    Initial commit

---
+++ assignment4/assignment4/MTStockMaintenance.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace assignment4
+{
+    partial class MTStockMaintenance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstStockRecords = new System.Windows.Forms.ListBox();
+            this.lblStockID = new System.Windows.Forms.Label();
+            this.txtStockID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.lblMinutes = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.RichTextBox();
+            this.chkIsProcedure = new System.Windows.Forms.CheckBox();
+            this.lblMessages = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstStockRecords
+            // 
+            this.lstStockRecords.FormattingEnabled = true;
+            this.lstStockRecords.ItemHeight = 20;
+            this.lstStockRecords.Location = new System.Drawing.Point(12, 16);
+            this.lstStockRecords.Name = "lstStockRecords";
+            this.lstStockRecords.Size = new System.Drawing.Size(246, 464);
+            this.lstStockRecords.TabIndex = 0;
+            this.lstStockRecords.SelectedIndexChanged += new System.EventHandler(this.lstStockRecords_SelectedIndexChanged);
+            // 
+            // lblStockID
+            // 
+            this.lblStockID.AutoSize = true;
+            this.lblStockID.Location = new System.Drawing.Point(287, 30);
+            this.lblStockID.Name = "lblStockID";
+            this.lblStockID.Size = new System.Drawing.Size(75, 20);
+            this.lblStockID.TabIndex = 1;
+            this.lblStockID.Text = "Stock ID:";
+            // 
+            // txtStockID
+            // 
+            this.txtStockID.Location = new System.Drawing.Point(386, 27);
+            this.txtStockID.Name = "txtStockID";
+            this.txtStockID.ReadOnly = true;
+            this.txtStockID.Size = new System.Drawing.Size(330, 26);
+            this.txtStockID.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(386, 59);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(330, 26);
+            this.txtName.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(287, 62);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(55, 20);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(386, 218);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(330, 26);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(287, 221);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(48, 20);
+            this.lblPrice.TabIndex = 5;
+            this.lblPrice.Text = "Price:";
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.Location = new System.Drawing.Point(386, 250);
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(330, 26);
+            this.txtMinutes.TabIndex = 5;
+            // 
+            // lblMinutes
+            // 
+            this.lblMinutes.AutoSize = true;
+            this.lblMinutes.Location = new System.Drawing.Point(287, 253);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(69, 20);
+            this.lblMinutes.TabIndex = 7;
+            this.lblMinutes.Text = "Minutes:";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(287, 102);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(93, 20);
+            this.lblDescription.TabIndex = 9;
+            this.lblDescription.Text = "Description:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(386, 91);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(330, 121);
+            this.txtDescription.TabIndex = 3;
+            this.txtDescription.Text = "";
+            // 
+            // chkIsProcedure
+            // 
+            this.chkIsProcedure.AutoSize = true;
+            this.chkIsProcedure.Location = new System.Drawing.Point(394, 301);
+            this.chkIsProcedure.Name = "chkIsProcedure";
+            this.chkIsProcedure.Size = new System.Drawing.Size(125, 24);
+            this.chkIsProcedure.TabIndex = 6;
+            this.chkIsProcedure.Text = "Is Procedure";
+            this.chkIsProcedure.UseVisualStyleBackColor = true;
+            // 
+            // lblMessages
+            // 
+            this.lblMessages.AutoSize = true;
+            this.lblMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessages.ForeColor = System.Drawing.Color.Red;
+            this.lblMessages.Location = new System.Drawing.Point(287, 380);
+            this.lblMessages.Name = "lblMessages";
+            this.lblMessages.Size = new System.Drawing.Size(24, 20);
+            this.lblMessages.TabIndex = 12;
+            this.lblMessages.Text = "...";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(744, 39);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(133, 43);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear Inputs";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(744, 91);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(133, 43);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(744, 150);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(133, 43);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(744, 210);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(133, 43);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.ForeColor = System.Drawing.Color.Red;
+            this.btnClose.Location = new System.Drawing.Point(744, 270);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(133, 43);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // MTStockMaintenance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 502);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblMessages);
+            this.Controls.Add(this.chkIsProcedure);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.lblMinutes);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtStockID);
+            this.Controls.Add(this.lblStockID);
+            this.Controls.Add(this.lstStockRecords);
+            this.Name = "MTStockMaintenance";
+            this.Text = "Stock Maintenance";
+            this.Load += new System.EventHandler(this.MTStockMaintenance_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstStockRecords;
+        private System.Windows.Forms.Label lblStockID;
+        private System.Windows.Forms.TextBox txtStockID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.Label lblMinutes;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.RichTextBox txtDescription;
+        private System.Windows.Forms.CheckBox chkIsProcedure;
+        private System.Windows.Forms.Label lblMessages;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ assignment4/assignment4/MTStockMaintenance.cs
@@ -0,0 +1,204 @@
+﻿/*
+ * Program ID: assignment4
+ * 
+ * Purpose: To manage and maintain stock for a beauty shop
+ * 
+ * Revision History
+ *      created by Maria Tran on April 2, 2021
+ *      updated by Maria Tran on April 9, 2021
+*/
+using MTClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment4
+{
+    public partial class MTStockMaintenance : Form
+    {
+        public MTStockMaintenance()
+        {
+            InitializeComponent();
+        }
+
+        Boolean isAdd = false;
+
+        //Clears input fields
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtStockID.Text = "0";
+            txtName.Text = "";
+            txtDescription.Text = "";
+            txtPrice.Text = "";
+            txtMinutes.Text = "";
+            chkIsProcedure.Checked = false;
+            txtStockID.Enabled = true;
+            isAdd = true;
+            lblMessages.Text = "Input fields have been cleared\n";
+        }
+
+        //Saves the record into the text file
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            lblMessages.Text = "";
+            double price = 0;
+            Int32 minutes = 0;
+
+            if (txtStockID.Text == "0")
+            {
+                isAdd = true;
+            }
+            else
+            {
+                isAdd = false;
+            }
+
+            MTStock stock = new MTStock(Convert.ToInt32(txtStockID.Text));
+
+            stock.Name = txtName.Text;
+            stock.Description = txtDescription.Text;
+
+            if (double.TryParse(txtPrice.Text, out price))
+            {
+                stock.Price = price;
+            }
+            else
+            {
+                lblMessages.Text += "Price is not numeric\n";
+            }
+
+            if (Int32.TryParse(txtMinutes.Text, out minutes))
+            {
+                stock.Minutes = minutes;
+            }
+            else
+            {
+                lblMessages.Text += "Minutes is not numeric\n";
+            }
+
+            stock.IsProcedure = chkIsProcedure.Checked;
+
+            try
+            {
+                if (isAdd == true && txtStockID.Text == "0")
+                {
+                    stock.MTAdd(stock.StockID);
+                }
+                else if (isAdd == false)
+                {
+                    stock.MTUpdate(stock.StockID);
+                }
+
+                LoadListBox();
+                lstStockRecords.SelectedValue = stock.StockID;
+
+                isAdd = false;
+            }
+            catch (Exception ex)
+            {
+                lblMessages.Text += "Problems with saving/updating record: \n" + ex.Message + "\n";
+            }
+
+            if (lblMessages.Text != "")
+            {
+                return;
+            }
+        }
+
+        //Deletes a selected record and re-selects the record after it
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtStockID.Enabled == false)
+                {
+                    MTStock.MTDelete(txtStockID.Text);
+                    string name = txtName.Text;
+
+                    LoadListBox();
+                    MTStock stock = MTStock.MTGetByDescription(name);
+                    if (lstStockRecords.SelectedIndex != 0)
+                    {
+                        lstStockRecords.SelectedValue = stock.StockID;
+                    }
+                }
+                else
+                {
+                    lblMessages.Text = "Please re-select a stock item to delete\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessages.Text = "Problem trying to delete stock: \n" + ex.Message + "\n";
+            }
+        }
+
+        //Reverts input areas to their values before it was cleared or modified
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if(lstStockRecords.SelectedIndex == 0)
+            {
+                lstStockRecords_SelectedIndexChanged(sender, e);
+            }
+            else
+            {
+                lblMessages.Text = "Error with cancelling input areas\n";
+            }
+        }
+
+        //Closes the form
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        //Reload listbox with stock
+        private void LoadListBox()
+        {
+            List<MTStock> stocks = MTStock.MTGetStocks();
+            stocks = stocks.OrderBy(a => a.Name).ToList();
+
+            lstStockRecords.DisplayMember = "Name";
+            lstStockRecords.ValueMember = "StockID";
+            lstStockRecords.DataSource = stocks;
+        }
+
+        //A record selected in the list will reload the input areas
+        private void lstStockRecords_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtStockID.Enabled = false;
+            try
+            {
+                MTStock record = MTStock.MTGetByStockID(Convert.ToInt32(lstStockRecords.SelectedValue));
+
+                txtStockID.Text = record.StockID.ToString();
+                txtName.Text = record.Name;
+                txtDescription.Text = record.Description;
+                txtPrice.Text = record.Price.ToString();
+                txtMinutes.Text = record.Minutes.ToString();
+                chkIsProcedure.Checked = record.IsProcedure;
+            }
+            catch (Exception ex)
+            {
+                lblMessages.Text = "Problems with loading list: \n" + ex.Message + "\n";
+            }
+
+        }
+
+        //Loads listbox when form loads
+        private void MTStockMaintenance_Load(object sender, EventArgs e)
+        {
+            LoadListBox();
+            if (txtStockID.Text == "")
+            {
+                txtStockID.Text = "0";
+            }
+        }
+    }
+}
